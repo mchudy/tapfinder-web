48cb36d737d777ae441708cf760695d0fa74e3a7
Marcin Chudy <marcinchudy94@gmail.com>
2016-06-15 12:36:45 +0200

    Add minimum experience to Ranks table

---
+++ src/PubApp.Migrations/Scripts/160615122520_Rank_AddMinExperience.sql
@@ -0,0 +1,20 @@
+ALTER TABLE [dbo].[Ranks] 
+ADD [MinExperience] int NOT NULL DEFAULT 0;
+
+UPDATE [dbo].[Ranks] SET [MinExperience] = 0 
+WHERE [Id] = 1;
+
+UPDATE [dbo].[Ranks] SET [MinExperience] = 50
+WHERE [Id] = 2;
+
+UPDATE [dbo].[Ranks] SET [MinExperience] = 100
+WHERE [Id] = 3;
+
+UPDATE [dbo].[Ranks] SET [MinExperience] = 200
+WHERE [Id] = 4;
+
+UPDATE [dbo].[Ranks] SET [MinExperience] = 500
+WHERE [Id] = 5;
+
+UPDATE [dbo].[Ranks] SET [MinExperience] = 1000
+WHERE [Id] = 6;
